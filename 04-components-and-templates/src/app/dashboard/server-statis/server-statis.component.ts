0a57ab776121f247ea42e6a0a29661c93c303612
Mateusz Kierepka <mateusz.kierepka03@gmail.com>
2025-05-05 09:35:40 +0200

    update

--- 04-components-and-templates/src/app/dashboard/server-statis/server-statis.component.ts
+++ 04-components-and-templates/src/app/dashboard/server-statis/server-statis.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-statis',
@@ -7,6 +7,35 @@ import { Component } from '@angular/core';
   templateUrl: './server-statis.component.html',
   styleUrl: './server-statis.component.css'
 })
-export class ServerStatisComponent {
-  currentStatus = 'online';
+export class ServerStatisComponent implements OnInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {}
+  
+  ngOnInit() {
+    console.log('ON INIT');
+    const inverval = setInterval(() => {
+      const rnd = Math.random(); // 0 - 0.9999999999999999
+      if (rnd > 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd > 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(inverval);
+    });
+  }
+
+  ngAfterViewInit() {
+    console.log('AFTER VIEW INIT');
+  }
+
+  // ngOnDestroy() {
+  //   clearTimeout(this.interval);
+  // }
 }
